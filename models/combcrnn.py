81d1f2cceb86835da4320c85caf81685998cff7e
Rohit Gajawada <rohit.gajawada@gmail.com>
2017-10-16 12:12:24 +0530

    lets start!

---
+++ models/combcrnn.py
@@ -0,0 +1,143 @@
+import torch.nn as nn
+
+class BidirectionalLSTM(nn.Module):
+    def __init__(self, nIn, nHidden, nOut):
+        super(BidirectionalLSTM, self).__init__()
+        self.rnn = nn.LSTM(nIn, nHidden, bidirectional=True)
+        self.embedding = nn.Linear(nHidden * 2, nOut)
+
+    def forward(self, input):
+        recurrent, _ = self.rnn(input)
+        T, b, h = recurrent.size()
+        t_rec = recurrent.view(T * b, h)
+        output = self.embedding(t_rec)  # [T * b, nOut]
+        output = output.view(T, b, -1)
+        return output
+
+class CRNN(nn.Module):
+
+    def __init__(self, imgH, nc, nclass, nh, n_rnn=2, leakyRelu=False):
+        super(CRNN, self).__init__()
+        assert imgH % 16 == 0, 'imgH has to be a multiple of 16'
+
+        self.relu = nn.ReLU()
+
+        #parallel net1
+        self.conv1a = nn.Conv2d(1, 6, kernel_size=(1, 3), stride=(1, 1), padding=(0, 1))
+        self.bn1a = nn.BatchNorm2d(6)
+        self.mp1a = nn.MaxPool2d((1, 3), (1, 2))
+        self.conv2a = nn.Conv2d(6, 16, kernel_size=(2, 3), stride=(1, 1), padding=(0, 0))
+        self.bn2a = nn.BatchNorm2d(16)
+        self.mp2a = nn.MaxPool2d((1, 3), (1, 2))
+
+        #parallel net2
+        self.conv1b = nn.Conv2d(1, 6, kernel_size=(1, 3), stride=(1, 1), padding=(0, 1))
+        self.bn1b = nn.BatchNorm2d(6)
+        self.mp1b = nn.MaxPool2d((1, 3), (1, 2))
+        self.conv2b = nn.Conv2d(6, 16, kernel_size=(2, 3), stride=(1, 1), padding=(0, 1))
+        self.bn2b = nn.BatchNorm2d(16)
+        self.mp2b = nn.MaxPool2d((1, 3), (1, 2))
+
+        #main net
+        self.conv0 = nn.Conv2d(nc, 64, kernel_size=3, stride=1, padding=1)
+        self.mp0 = nn.MaxPool2d(2, 2)
+        self.conv1 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.mp1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(256)
+        self.conv3 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.mp3 = nn.MaxPool2d((2, 2), (2, 1), (0, 1))
+        self.conv4 = nn.Conv2d(260, 512, kernel_size=3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(512)
+        self.conv5 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.mp5 = nn.MaxPool2d((2, 2), (2, 1), (0, 1))
+        self.conv6 = nn.Conv2d(520, 520, kernel_size=2, stride=1, padding=0)
+        self.bn6 = nn.BatchNorm2d(520)
+
+        self.rnn = nn.Sequential(
+            BidirectionalLSTM(520, nh, nh),
+            BidirectionalLSTM(nh, nh, nclass))
+
+    def forward(self, x, stk):
+
+        sx, sy, sz = torch.split(stk, 1, 1)
+        sx, sy, sz = sx.contiguous().float().view(1, 1, 1, -1), sy.contiguous().float().view(1, 1, 1, -1), sz.contiguous().float().view(1, 1, 1, -1)
+
+        stk1 = torch.cat((sx, sy), dim=2)
+        # stk2 = torch.cat((sx, sy, sz), dim=2)
+
+        xa = self.relu(self.bn1a(self.conv1a(stk1)))
+        print xa.size()
+        xa = self.mp1a(xa)
+        print xa.size()
+        xa = self.relu(self.bn2a(self.conv2a(xa)))
+        print xa.size()
+        xa = self.mp2a(xa)
+        print xa.size()
+
+        print "-----------"
+
+        xb = self.relu(self.bn1b(self.conv1b(stk1)))
+        print xb.size()
+        xb = self.mp1b(xb)
+        print xb.size()
+        xb = self.relu(self.bn2b(self.conv2b(xb)))
+        print xb.size()
+        xb = self.mp2b(xb)
+        print xb.size()
+
+        print "------------"
+
+        xa = xa.view(-1, 4, 4, 129)
+        xb = xb.view(-1, 8, 2, 130)
+
+        print xa.size()
+        print xb.size()
+
+        x = self.relu(self.conv0(x))
+        x = self.mp0(x)
+        print(x.size())
+        x = self.relu(self.conv1(x))
+        x = self.mp1(x)
+        print(x.size())
+        x = self.relu(self.bn2(self.conv2(x)))
+        x = self.relu(self.conv3(x))
+        x = self.mp3(x)
+        x = torch.cat((x, xa), dim=1)
+        print(x.size())
+
+        x = self.relu(self.bn4(self.conv4(x)))
+        x = self.relu(self.conv5(x))
+        x = self.mp5(x)
+        x = torch.cat((x, xb), dim=1)
+        print(x.size())
+
+        x = self.relu(self.bn6(self.conv6(x)))
+        print(x.size())
+        b, c, h, w = x.size()
+        assert h == 1, "the height of conv must be 1"
+        x = x.squeeze(2)
+        x = x.permute(2, 0, 1)
+
+        output = self.rnn(x)
+
+        return output
+
+import torch
+from torch.autograd import Variable
+import numpy as np
+import cPickle
+
+path = './words/testword1_1_new.p'
+f = open(path, 'rb')
+stk = cPickle.load(f)
+f.close()
+stk = np.array(stk, dtype=float)
+
+stk = Variable(torch.from_numpy(stk)).float()
+
+stk = Variable(torch.randn(524, 3))
+im = Variable(torch.randn(1, 3, 32, 512))
+net = CRNN(32, 3, 10, 256)
+
+output = net(im, stk)
---
+++ utils.py
@@ -0,0 +1,185 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import collections
+import pickle
+
+
+class strLabelConverter(object):
+    """Convert between str and label.
+
+    NOTE:
+        Insert `blank` to the alphabet for CTC.
+
+    Args:
+        alphabet (str): set of the possible characters.
+        ignore_case (bool, default=True): whether or not to ignore all of the case.
+    """
+
+    def __init__(self, alphabet, ignore_case=True):
+        self._ignore_case = ignore_case
+        if self._ignore_case:
+            alphabet = alphabet.lower()
+        self.alphabet = alphabet + '-'  # for `-1` index
+
+        self.dict = {}
+        for i, char in enumerate(alphabet):
+            # NOTE: 0 is reserved for 'blank' required by wrap_ctc
+            self.dict[char] = i + 1
+
+        # print self.dict
+
+    def encode(self, text):
+        """Support batch or single str.
+
+        Args:
+            text (str or list of str): texts to convert.
+
+        Returns:
+            torch.IntTensor [length_0 + length_1 + ... length_{n - 1}]: encoded texts.
+            torch.IntTensor [n]: length of each text.
+        """
+
+        # print text
+        t = []
+        l = []
+        for i in range(len(text)):
+            t += pickle.loads(text[i])
+            l.append(len(pickle.loads(text[i])))
+        # print t
+        # print l
+        return (torch.IntTensor(t), torch.IntTensor(l))
+
+    def unpickle(self,text):
+        t = []
+        for i in range(len(text)):
+            t.append(pickle.loads(text[i]))
+        return t
+
+    def decode(self, t, length, raw=False):
+        """Decode encoded texts back into strs.
+
+        Args:
+            torch.IntTensor [length_0 + length_1 + ... length_{n - 1}]: encoded texts.
+            torch.IntTensor [n]: length of each text.
+
+        Raises:
+            AssertionError: when the texts and its length does not match.
+
+        Returns:
+            text (str or list of str): texts to convert.
+        """
+        # if length.numel() == 1:
+        #     length = length[0]
+        #     assert t.numel() == length, "text with length: {} does not match declared length: {}".format(t.numel(), length)
+        #     if raw:
+        #         return ''.join([self.alphabet[i - 1] for i in t])
+        #     else:
+        #         char_list = []
+        #         for i in range(length):
+        #             if t[i] != 0 and (not (i > 0 and t[i - 1] == t[i])):
+        #                 char_list.append(self.alphabet[t[i] - 1])
+        #         return ''.join(char_list)
+        # else:
+        #     # batch mode
+        #     assert t.numel() == length.sum(), "texts with length: {} does not match declared length: {}".format(t.numel(), length.sum())
+        #     texts = []
+        #     index = 0
+        #     for i in range(length.numel()):
+        #         l = length[i]
+        #         texts.append(
+        #             self.decode(
+        #                 t[index:index + l], torch.IntTensor([l]), raw=raw))
+        #         index += l
+        lis = []
+        pntr = 0
+        if raw == False:
+            for i in length:
+                # print (t[pntr:pntr+i])
+                text = ''
+                for j in range(i):
+                    if t[pntr+j] == 0:
+                        continue
+                    if text != '':
+                        val = text.split(',')[-2]
+                        if int(val) == t[pntr+j]:
+                            continue
+                    text += str(t[pntr+j])+','
+                # print (text)
+                pntr += i
+                lis.append(text[:-1])
+        else:
+            for i in length:
+                text = ''
+                for j in range(i):
+                    text += str(t[pntr+j])+','
+                pntr += i
+                lis.append(text[:-1])
+        return lis
+
+
+class averager(object):
+    """Compute average for `torch.Variable` and `torch.Tensor`. """
+
+    def __init__(self):
+        self.reset()
+
+    def add(self, v):
+        if isinstance(v, Variable):
+            count = v.data.numel()
+            v = v.data.sum()
+        elif isinstance(v, torch.Tensor):
+            count = v.numel()
+            v = v.sum()
+
+        self.n_count += count
+        self.sum += v
+
+    def reset(self):
+        self.n_count = 0
+        self.sum = 0
+
+    def val(self):
+        res = 0
+        if self.n_count != 0:
+            res = self.sum / float(self.n_count)
+        return res
+
+
+def oneHot(v, v_length, nc):
+    batchSize = v_length.size(0)
+    maxLength = v_length.max()
+    v_onehot = torch.FloatTensor(batchSize, maxLength, nc).fill_(0)
+    acc = 0
+    for i in range(batchSize):
+        length = v_length[i]
+        label = v[acc:acc + length].view(-1, 1).long()
+        v_onehot[i, :length].scatter_(1, label, 1.0)
+        acc += length
+    return v_onehot
+
+
+def loadData(v, data):
+    v.data.resize_(data.size()).copy_(data)
+
+
+def prettyPrint(v):
+    print('Size {0}, Type: {1}'.format(str(v.size()), v.data.type()))
+    print('| Max: %f | Min: %f | Mean: %f' % (v.max().data[0], v.min().data[0],
+                                              v.mean().data[0]))
+
+
+def assureRatio(img):
+    """Ensure imgH <= imgW."""
+    b, c, h, w = img.size()
+    if h > w:
+        main = nn.UpsamplingBilinear2d(size=(h, h), scale_factor=None)
+        img = main(img)
+    return img
+
+if __name__ == '__main__':
+    test = strLabelConverter('abcdefghijklmnopqrstuvwxyz')
+    print test.encode([[1,2,3,4],[5,6,7,8]])
